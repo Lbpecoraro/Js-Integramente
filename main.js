53c0cc4ce3a024d2418d78e23acbf3f407ef084b
lbpecoraro <lbpecoraro@gmail.com>
2021-11-20 17:05:53 -0300

    v1.0.0

---
+++ main.js
@@ -0,0 +1,13 @@
+alert("Hola, procederemos a pedirte información personal, no te preocupes, esta no se mostrará de manera pública a menos que sea requerido para aprobar en coder, gracias")
+let nombre= prompt("Ingrese su hermoso nombre que eligieron sus papás.");
+let apellido= prompt ("Ingrese su apellido aunque no puede ser tan hermoso por que eso no se elije que yo sepa.");
+alert(`Hola ${nombre} ${apellido}, solo nos falta una ultima información que pedirte`);
+let anoDeNacimiento= prompt("Ingresa el año en que naciste");
+let anoDeNacimientoParseado= parseInt(anoDeNacimiento);
+let anoActual= 2021;
+let edad= anoActual - anoDeNacimientoParseado;
+alert(`Info personal obtenida:
+Nombre: ${nombre}
+Apellido: ${apellido}
+Edad: ${edad}
+`)
