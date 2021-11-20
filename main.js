d055fd0b0476977c4cacefab05190a0f00fa9c53
lbpecoraro <lbpecoraro@gmail.com>
2021-11-20 17:08:36 -0300

    v1.0.0

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+/**
+ * @challenge: Crear un algoritmo JS simple
+ * 
+ * @version: v1.0.0
+ * @author: Lucia Pecoraro
+ * @fecha: 20/11/2021
+ *
+ * History:
+
+ */
+
+
 alert("Hola, procederemos a pedirte información personal, no te preocupes, esta no se mostrará de manera pública a menos que sea requerido para aprobar en coder, gracias")
 let nombre= prompt("Ingrese su hermoso nombre que eligieron sus papás.");
 let apellido= prompt ("Ingrese su apellido aunque no puede ser tan hermoso por que eso no se elije que yo sepa.");
